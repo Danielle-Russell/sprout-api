f0705161d6820257ff07447cf4b4e8b967beec0f
Danielle-Russell <67922266+DanielleRussell-beep@users.noreply.github.com>
2020-11-24 11:21:31 -0800

    changed console log to debug error

--- src/app.js
+++ src/app.js
@@ -65,8 +65,9 @@ app.use(helmet())
      app.use(function errorHandler(error, req, res, next) {
           let response
            if (NODE_ENV === 'production') {
-             response = { error: { message: 'server error' } }
-
+             //response = { error: { message: 'server error' } }
+             console.error(error)
+             response = { message: error.message, error }
            } else {
              console.error(error)
              response = { message: error.message, error }
