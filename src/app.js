4c5b05c64fd195e390c744946030eb15d956ad43
Danielle-Russell <67922266+DanielleRussell-beep@users.noreply.github.com>
2020-11-25 12:39:23 -0800

    added new file limit

--- src/app.js
+++ src/app.js
@@ -28,8 +28,8 @@ app.use(function (req, res, next) {
 });
 
 
-/*app.use(bodyParser.json({limit: "50mb"}));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));*/
+app.use(express.json({limit: '50mb'}));
+app.use(express.urlencoded({limit: '50mb'}));
 
 
 app.use("/api/sprouts", sprouts)
